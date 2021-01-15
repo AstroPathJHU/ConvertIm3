a3a08c826df4000edc4cb78bf54e9568dd5ac835
Ben Green <46007058+beng1290@users.noreply.github.com>
2021-01-15 17:24:59 -0500

    Add files via upload

---
+++ ConvertIM3Cohort.ps1
@@ -0,0 +1,26 @@
+﻿<# ---------------------------------------------------------
+For each sample directory not control extract the xml file 
+formats
+ ---------------------------------------------------------#>
+
+
+Function ConvertIM3Cohort {
+    param ( $root, $output_dir )
+    $sample_all = Get-ChildItem -Path $root -Directory
+    
+    #
+    foreach ($sub in $sample_all) {
+        $IM3_fd = $root + "\" + $sub.Name + "\im3\"
+        $ii = test-path -path $IM3_fd
+        #
+        IF (!$ii -or $sub.Name -like "Control*") {
+            Continue
+        }
+        M:\Im3Shred\ConvertIM3Path.ps1 $root $output_dir $sub -s -xml
+    }
+    #
+}
+
+ConvertIM3Cohort '\\bki04\Clinical_Specimen_2' '\\bki04\flatw_2'
+
+
